5ddb3484588967998ec7aaa2d1f691944a9eecbf
mherrmann1508 <mherrmann1508@gmail.com>
2020-03-11 23:56:17 +0100

    ADDED serializer files

---
+++ Package/BitSerialization/BitBuffer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.Dunkingmachine.BitSerialization
+{
+    public class BitBuffer
+    {
+        public enum Mode {Read,Write}
+        
+        public Mode BufferMode { get; private set; }
+
+        private byte[] _buffer;
+        private List<byte> _listBuffer;
+        private int _index;
+        private int _bitIndex;
+        private bool _closed;
+
+        public BitBuffer()
+        {
+            BufferMode = Mode.Write;
+            _listBuffer = new List<byte>{0x0};
+        }
+
+        public BitBuffer(byte[] byteBuffer)
+        {
+            BufferMode = Mode.Read;
+            _buffer = byteBuffer;
+        }
+
+        public ulong Read(int amount)
+        {
+            if(BufferMode != Mode.Read)
+                throw new Exception("BitBuffer not in read-mode!");
+            var leftInCurrentByte = 8 - _bitIndex;
+            ulong result;
+            if (amount <= leftInCurrentByte)
+            {
+                result =  ((ulong)_buffer[_index] >> (leftInCurrentByte-amount)) & ((1u << amount) - 1);
+                IncrementIndex(amount);
+            }
+            else
+            {
+                var leftToRead = amount-leftInCurrentByte;
+                result =  (ulong)_buffer[_index] & ((1u << leftInCurrentByte)-1);
+                IncrementIndex(leftInCurrentByte);
+                while (leftToRead > 8)
+                {
+                    result |= (ulong)_buffer[_index] << (amount - leftToRead);
+                    leftToRead -= 8;
+                    IncrementIndex(8);
+                }
+                
+                result |=  ((ulong)_buffer[_index] >> (8 - leftToRead)) << (amount - leftToRead);
+                IncrementIndex(leftToRead);
+            }
+            return result;
+        }
+
+        public void Write(ulong bits, int amount)
+        {
+            if(BufferMode != Mode.Write)
+                throw new Exception("BitBuffer not in write-mode!");
+            if(_closed)
+                throw new Exception("BitBuffer is closed!");
+            
+            var leftInCurrentByte = 8 - _bitIndex;
+            if (amount <= leftInCurrentByte)
+            {
+                _listBuffer[_index] = (byte) ((_listBuffer[_index] << amount) | (byte) bits & ((1 << amount) - 1));
+                IncrementIndex(amount);
+                return;
+            }
+            
+            _listBuffer[_index] = (byte) ((_listBuffer[_index] << leftInCurrentByte) | (byte) bits & ((1 << leftInCurrentByte) - 1));
+            IncrementIndex(leftInCurrentByte);
+            var leftToWrite = amount - leftInCurrentByte;
+            while (leftToWrite > 8)
+            {
+                _listBuffer[_index] = (byte) (bits >> (amount - leftToWrite));
+                IncrementIndex(8);
+                leftToWrite -= 8;
+            }
+
+            _listBuffer[_index] = (byte) (bits >> (amount - leftToWrite));
+            IncrementIndex(leftToWrite);
+        }
+
+        public byte[] GetBytes()
+        {
+            if(!_closed)
+                Close();
+            return _buffer;
+        }
+
+        private void Close()
+        {
+            _closed = true;
+            _listBuffer[_index] <<= 8 - _bitIndex;
+            _buffer = _listBuffer.ToArray();
+        }
+
+        private void IncrementIndex(int amount)
+        {
+            _bitIndex += amount;
+            while (_bitIndex > 7)
+            {
+                _bitIndex -= 8;
+                _index++;
+                if (BufferMode == Mode.Write)
+                    _listBuffer.Add(0x0);
+            }
+            
+        }
+    }
+}
---
+++ Package/BitSerialization/BitSerializer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+
+namespace com.Dunkingmachine.BitSerialization
+{
+    public class BitSerializer
+    {
+
+        private readonly BitBuffer _buffer;
+        public BitBuffer.Mode BufferMode => _buffer.BufferMode;
+
+        public BitSerializer()
+        {
+            _buffer = new BitBuffer();
+        }
+
+        public BitSerializer(byte[] bytes)
+        {
+            _buffer = new BitBuffer(bytes);
+        }
+
+        public byte[] GetBytes()
+        {
+            return _buffer.GetBytes();
+        }
+
+        
+        public void WriteVarInt(long value)
+        {
+            if (value >= 0)
+            {
+                _buffer.Write(0,1);
+            }
+            else
+            {
+                _buffer.Write(1,1);
+                value *= -1;
+            }
+            
+            do
+            {
+                _buffer.Write((ulong) (value & 127), 7);
+                value >>= 7;
+                _buffer.Write(value > 0 ? 1u : 0, 1);
+            } while (value > 0);
+        }
+
+        public long ReadVarInt()
+        {
+            var sign = _buffer.Read(1);
+            var value = 0L;
+            var shift = 0;
+            do
+            {
+                value |= (long) (_buffer.Read(7) << shift);
+                shift += 7;
+            } while (_buffer.Read(1) == 1);
+
+            if (sign == 1)
+                value *= -1;
+            return value;
+        }
+
+        public void WriteInt(int value, int bits)
+        {
+            if (value >= 0)
+            {
+                _buffer.Write(0, 1);
+                _buffer.Write((ulong) value, bits);
+            }
+            else
+            {
+                _buffer.Write(1, 1);
+                _buffer.Write((ulong) (value * -1), bits);
+            }
+        }
+
+        public int ReadInt(int bits)
+        {
+            var sign = _buffer.Read(1);
+            var value = (int) _buffer.Read(bits);
+            if (sign == 1)
+                value *= -1;
+            return value;
+        }
+
+        public void WriteBool(bool value)
+        {
+            _buffer.Write(value ? 1u : 0,1);
+        }
+
+        public bool ReadBool()
+        {
+            return _buffer.Read(1) == 1u;
+        }
+
+        public void WriteFloatLossless(float value)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            var littleEndian = BitConverter.IsLittleEndian;
+            for (var i = 0; i < 4; i++)
+            {
+                _buffer.Write(bytes[littleEndian ? i : 3 - i], 8);
+            }
+        }
+
+        public float ReadFloatLossless()
+        {
+            var littleEndian = BitConverter.IsLittleEndian;
+            var bytes = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                bytes[littleEndian ? i : 3 - i] = (byte) _buffer.Read(8);
+            }
+
+            return BitConverter.ToSingle(bytes, 0);
+        }
+
+        public void WriteString(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+            var length = bytes.Length;
+            if(length > short.MaxValue)
+                throw new Exception("String length exceeds allowed size!");
+            _buffer.Write(length > 127 ? 1U : 0, 1); //write category: 0 = small string, 1 = big string
+            _buffer.Write((ulong) length, length > 127 ? 15 : 7); //write length using bits allowed by category
+            foreach (var b in bytes)
+            {
+                _buffer.Write(b, 8);
+            }
+        }
+
+        public string ReadString()
+        {
+            var category = _buffer.Read(1);
+            var length = (int)_buffer.Read(category == 1 ? 15 : 7);
+            var bytes = new byte[length];
+            for (var i = 0; i < length; i++)
+            {
+                bytes[i] = (byte) _buffer.Read(8);
+            }
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
