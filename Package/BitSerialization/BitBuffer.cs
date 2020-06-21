a0ae94e47e96df9184673506c8515306950086c5
Erik Hegen <catulugames@gmail.com>
2020-06-21 14:05:11 +0200

    ADDED: documentation, exception handling and tests for bit buffer

--- Package/BitSerialization/BitBuffer.cs
+++ Package/BitSerialization/BitBuffer.cs
@@ -1,85 +1,140 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
+
+#endregion
 
 namespace com.Dunkingmachine.BitSerialization
 {
     public class BitBuffer
     {
-        public enum Mode {Read,Write}
-        
-        public Mode BufferMode { get; }
+        public enum Mode { Read, Write }
+
+        private readonly List<byte> _listBuffer;
+        private int _bitIndex;
 
         private byte[] _buffer;
-        private readonly List<byte> _listBuffer;
-        private int _index;
-        private int _bitIndex;
+        private readonly int _bufferLength; 
         private bool _closed;
+        private int _index;
 
-        public bool LastByte => _index == _buffer.Length -1;
-
+        /// <summary>
+        ///     Creates an empty, default BitBuffer with Write-Mode
+        /// </summary>
         public BitBuffer()
         {
             BufferMode = Mode.Write;
-            _listBuffer = new List<byte>{0x0};
+            _listBuffer = new List<byte> {0x0};
         }
 
+        /// <summary>
+        ///     Creates a default BitBuffer from the given byte buffer with Read-Mode
+        /// </summary>
+        /// <param name="byteBuffer"></param>
+        /// <exception cref="ArgumentNullException">Thrown when calling constructor with null value</exception>
         public BitBuffer(byte[] byteBuffer)
         {
             BufferMode = Mode.Read;
-            _buffer = byteBuffer;
+            _buffer = byteBuffer ??
+                      throw new ArgumentNullException($"Cannot initialize {nameof(BitBuffer)} with a null value!");
             _closed = true;
+            _bufferLength = _buffer.Length * 8;
         }
 
+        /// <summary>
+        ///     True when buffer reached the last byte
+        /// </summary>
+        public bool LastByte => _index == _buffer.Length - 1;
+
+        /// <summary>
+        ///     Buffer mode <see cref="Mode" /> is set when creating the buffer object and can not be changed afterwards!
+        /// </summary>
+        public Mode BufferMode { get; }
+
+        /// <summary>
+        ///     Skips the given amount of bits from the current buffer position by setting the current buffer index accordingly 
+        /// </summary>
+        /// <param name="amount">Number of bits to skip in this buffer</param>
         public void Skip(int amount)
         {
             IncrementIndex(amount);
         }
 
+        /// <summary>
+        ///     Reads the given amount of bits from the buffer and returns their binary value  
+        /// </summary>
+        /// <param name="amount">Amount of bits that should be read</param>
+        /// <returns>
+        ///     Binary value of the amount of the specified bits
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     Throws when not in read mode.
+        ///     Throws when <paramref name="amount"/> is bigger than size of an ulong
+        /// </exception>
         public ulong Read(int amount)
         {
-            if(BufferMode != Mode.Read)
-                throw new Exception("BitBuffer not in read-mode!");
+            if (BufferMode != Mode.Read)
+                throw new InvalidOperationException("BitBuffer not in read-mode!");
+
+            if (amount > sizeof(ulong) * 8)
+                throw new InvalidOperationException(
+                    $"Cannot read more than {sizeof(ulong) * 8} bits at once. Amount of bits specified: {amount}");
+            
+            if (_index + amount > _bufferLength)
+                throw new IndexOutOfRangeException("Cannot read more bits from buffer than that are left!\n" +
+                                                   $"Current index: {_index}, buffer length: {_bufferLength}, amount: {amount}");
+            
             var leftInCurrentByte = 8 - _bitIndex;
             ulong result;
             if (amount <= leftInCurrentByte)
             {
-                result =  ((ulong)_buffer[_index] >> (leftInCurrentByte-amount)) & ((1u << amount) - 1);
+                result = ((ulong) _buffer[_index] >> (leftInCurrentByte - amount)) & ((1u << amount) - 1);
                 IncrementIndex(amount);
             }
             else
             {
-                var leftToRead = amount-leftInCurrentByte;
-                result =  (ulong)_buffer[_index] & ((1u << leftInCurrentByte)-1);
+                var leftToRead = amount - leftInCurrentByte;
+                result = (ulong) _buffer[_index] & ((1u << leftInCurrentByte) - 1);
                 IncrementIndex(leftInCurrentByte);
                 while (leftToRead > 8)
                 {
-                    result |= (ulong)_buffer[_index] << (amount - leftToRead);
+                    result |= (ulong) _buffer[_index] << (amount - leftToRead);
                     leftToRead -= 8;
                     IncrementIndex(8);
                 }
-                
-                result |=  ((ulong)_buffer[_index] >> (8 - leftToRead)) << (amount - leftToRead);
+
+                result |= ((ulong) _buffer[_index] >> (8 - leftToRead)) << (amount - leftToRead);
                 IncrementIndex(leftToRead);
             }
+
             return result;
         }
 
+        /// <summary>
+        ///     Writes a value for the given amount of bits. For performance reasons it's not being checked
+        ///     whether the amount of bits is sufficient for the given value 
+        /// </summary>
+        /// <param name="bits">value to write into the buffer</param>
+        /// <param name="amount">Amount of bits this value shall reserve</param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void Write(ulong bits, int amount)
         {
-            if(BufferMode != Mode.Write)
-                throw new Exception("BitBuffer not in write-mode!");
-            if(_closed)
-                throw new Exception("BitBuffer is closed!");
-            
+            if (BufferMode != Mode.Write)
+                throw new InvalidOperationException("BitBuffer not in write-mode!");
+            if (_closed)
+                throw new InvalidOperationException("BitBuffer is closed!");
+
             var leftInCurrentByte = 8 - _bitIndex;
             if (amount <= leftInCurrentByte)
             {
-                _listBuffer[_index] = (byte) ((_listBuffer[_index] << amount) | (byte) bits & ((1 << amount) - 1));
+                _listBuffer[_index] = (byte) ((_listBuffer[_index] << amount) | ((byte) bits & ((1 << amount) - 1)));
                 IncrementIndex(amount);
                 return;
             }
-            
-            _listBuffer[_index] = (byte) ((_listBuffer[_index] << leftInCurrentByte) | (byte) bits & ((1 << leftInCurrentByte) - 1));
+
+            _listBuffer[_index] = (byte) ((_listBuffer[_index] << leftInCurrentByte) |
+                                          ((byte) bits & ((1 << leftInCurrentByte) - 1)));
             IncrementIndex(leftInCurrentByte);
             var leftToWrite = amount - leftInCurrentByte;
             while (leftToWrite > 8)
@@ -93,15 +148,21 @@ namespace com.Dunkingmachine.BitSerialization
             IncrementIndex(leftToWrite);
         }
 
+        /// <summary>
+        ///     If not yet closed, closes the buffer and returns its bytes 
+        /// </summary>
+        /// <returns></returns>
         public byte[] GetBytes()
         {
-            if(!_closed)
+            if (!_closed)
                 Close();
             return _buffer;
         }
 
         private void Close()
         {
+            if (_closed) return;
+            
             _closed = true;
             _listBuffer[_index] <<= 8 - _bitIndex;
             _buffer = _listBuffer.ToArray();
@@ -117,7 +178,6 @@ namespace com.Dunkingmachine.BitSerialization
                 if (BufferMode == Mode.Write)
                     _listBuffer.Add(0x0);
             }
-            
         }
     }
 }
--- Package/Tests/BitBufferTests.cs
+++ Package/Tests/BitBufferTests.cs
@@ -8,27 +8,48 @@ namespace MyPackages.BitSerialization.Tests
     public class BitBufferTests
     {
         [Test]
-        public void Read_ModeIsWrite_ThrowsException()
+        public void Read_ModeIsWrite_ThrowsInvalidOperationException()
         {
             // Create empty buffer -> mode is Mode.Write new byte[]{0x00, 0xea}
             var buffer = new BitBuffer();
-            Assert.Throws<Exception>(() => buffer.Read(4));
+            Assert.Throws<InvalidOperationException>(() => buffer.Read(4));
         }
         
         [Test]
-        public void Write_ModeIsRead_ThrowsException()
+        public void Read_MoreBitsThenBufferContains_ThrowsIndexOutOfRangeException()
+        {
+            var buffer = new BitBuffer(new byte[0x0]);
+            Assert.Throws<IndexOutOfRangeException>(() => buffer.Read(4));
+        }
+        
+        [Test]
+        public void Read_AmountBiggerThanAllowed_ThrowsInvalidOperationException()
+        {
+            var buffer = new BitBuffer(new byte[0x0]);
+            Assert.Throws<InvalidOperationException>(() => buffer.Read(sizeof(ulong) * 8 + 1));
+        }
+        
+        [Test]
+        public void Write_ModeIsRead_ThrowsInvalidOperationException()
         {
             // Create non-empty buffer -> mode is Mode.Read
             var buffer = new BitBuffer(new byte[]{0x00, 0xea});
-            Assert.Throws<Exception>(() => buffer.Write(123, 4));
+            Assert.Throws<InvalidOperationException>(() => buffer.Write(123, 4));
         }
         
         [Test]
-        public void Write_BufferAlreadyClosed_ThrowsException()
+        public void Write_BufferAlreadyClosed_ThrowsInvalidOperationException()
         {
             var buffer = new BitBuffer();
             buffer.GetBytes();
-            Assert.Throws<Exception>(() => buffer.Write(123, 4));
+            Assert.Throws<InvalidOperationException>(() => buffer.Write(123, 4));
+        }
+
+        [Test]
+        public void CreateReadBuffer_BufferIsNull_ThrowsArgumentNullException()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new BitBuffer(null));
         }
     }
 }
